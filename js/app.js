7ee2d403b7da2bdad57aabd6e2a797309bc0d713
Erick Oda Coulter <erickodac@hotmail.com>
2021-09-03 17:37:43 -0300

    Mudança de cor na NavBar

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const hamburgerBtn = document.querySelector('.header___hamburger');
 const hamburgerMenu = document.querySelector('.hamburger___menu');
+const navbar = document.querySelector('.header___container')
 //const menuBtn = document.querySelector('.has-fade');
 
 hamburgerBtn.addEventListener('click', function(){
@@ -18,3 +19,29 @@ hamburgerBtn.addEventListener('click', function(){
     }
 
 })
+
+/*function scrollAnimation(){
+    if (window.scrollY > 10){
+        console.log("scroll");
+        navbar.classList.remove('background-out');
+        navbar.classList.add('background-in');
+    }
+    else {
+        console.log("scroll");
+        navbar.classList.remove('background-in');
+        navbar.classList.add('background-out');
+    }
+}*/
+
+setInterval(function scrollAnimation(){
+    if (window.scrollY > 10){
+        //console.log("scroll");
+        navbar.classList.remove('background-out');
+        navbar.classList.add('background-in');
+    }
+    else {
+        //console.log("not-scrolled");
+        navbar.classList.remove('background-in');
+        navbar.classList.add('background-out');
+    }
+}, 100);
