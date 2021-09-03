b0ae715aa20955cda5c35816ed78ce280b875c00
Erick Oda Coulter <erickodac@hotmail.com>
2021-09-03 01:17:37 -0300

    last of day

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
-const menuBtn = document.querySelector('.has-fade');
+const hamburgerBtn = document.querySelector('.header___hamburger');
+const hamburgerMenu = document.querySelector('.hamburger___menu');
+//const menuBtn = document.querySelector('.has-fade');
 
-menuBtn.addEventListener('click', function(){
+hamburgerBtn.addEventListener('click', function(){
 
-    console.log('click')
+    console.log('click');
 
+    if (hamburgerMenu.classList.contains('open')){
+        hamburgerMenu.classList.remove('open');
+        hamburgerMenu.classList.add('animate-out');
+        hamburgerMenu.classList.remove('animate-in');
+    }
+    else{
+        hamburgerMenu.classList.add('open');
+        hamburgerMenu.classList.add('animate-in');
+        hamburgerMenu.classList.remove('animate-out');
+    }
 
 })
